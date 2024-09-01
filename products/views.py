1a8283f6a564af242e5f87b9245cdb223bacedb6
Taher Mahmood <taher.ccg@gmail.com>
2024-09-01 10:09:57 +0000

    Add: Product views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import HennaProduct
 
 # Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = HennaProduct.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
