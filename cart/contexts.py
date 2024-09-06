84bbb3a869e76281a144a95468260b214b08506e
Taher Mahmood <taher.ccg@gmail.com>
2024-09-06 18:20:19 +0000

    Refactor: update context processor and cart.html

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,28 +1,44 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import HennaProduct
 
 def cart_contents(request):
+    """ Retrieve the contents of the cart and calculate totals and delivery costs """
 
     cart_items = []
-    total = 0
+    total = Decimal('0.00')
     product_count = 0
+    cart = request.session.get('cart', {})
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(HennaProduct, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    # Calculate delivery and free delivery delta
+    if total < Decimal(settings.FREE_DELIVERY_THRESHOLD):
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE) / Decimal('100')
+        free_delivery_delta = Decimal(settings.FREE_DELIVERY_THRESHOLD) - total
     else:
-        delivery = 0
-        free_delivery_delta = 0
-    
+        delivery = Decimal('0.00')
+        free_delivery_delta = Decimal('0.00')
+
     grand_total = delivery + total
-    
+
+    # Prepare context dictionary
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'free_delivery_threshold': Decimal(settings.FREE_DELIVERY_THRESHOLD),
         'grand_total': grand_total,
     }
 
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import HennaProduct
 
 def view_cart(request):
     """Get to return shopping cart"""
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping cart """
+
+    quantity = int(request.POST.get('quantity', 1)) 
+    redirect_url = request.POST.get('redirect_url')
+    
+    cart = request.session.setdefault('cart', {})
+    
+    cart[item_id] = cart.get(item_id, 0) + quantity
+
+    request.session.modified = True
+    
+    return redirect(redirect_url)
