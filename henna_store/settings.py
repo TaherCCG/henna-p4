e05e0c1824ef5ffa77b5803bd0f564e00dd94dc8
Taher Mahmood <taher.ccg@gmail.com>
2024-09-15 17:11:03 +0000

    Add: Added buttons for incrementing and decrementing quantities on the product details page.

--- henna_store/settings.py
+++ henna_store/settings.py
@@ -153,7 +153,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'products/static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
