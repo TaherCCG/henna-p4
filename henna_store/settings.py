350b2b9204048b8a0a732250d41fc6d1a2e5755d
Taher Mahmood <taher.ccg@gmail.com>
2024-09-18 15:55:46 +0000

    Add: Crispy forms apptemplates and VAT_RATE

--- henna_store/settings.py
+++ henna_store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+from decimal import Decimal
 from pathlib import Path
 if os.path.exists("env.py"):
     import env
@@ -41,7 +42,11 @@ INSTALLED_APPS = [
     'home',
     'products',
     "cart",
-    "checkout"
+    "checkout",
+    
+    # Other
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +62,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'henna_store.urls'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -71,7 +79,12 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
@@ -165,3 +178,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
+
+VAT_RATE = Decimal('0.20') 
