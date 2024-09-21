818d1964cd708cffa6cd06146ce6fafeb6ce518c
Taher Mahmood <taher.ccg@gmail.com>
2024-09-21 17:30:59 +0000

    Add: Checkout form data

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('update-delivery/<int:delivery_id>/', views.update_delivery, name='update_delivery'),
     path('checkout_success/<str:order_number>/', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
