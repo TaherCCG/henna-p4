ca5a50425d6705ea234cd6f5f7e399539d29876d
Taher Mahmood <taher.ccg@gmail.com>
2024-09-23 19:23:59 +0000

    FIx: clean up

--- henna_store/settings.py
+++ henna_store/settings.py
@@ -140,7 +140,6 @@ else:
 #     DATABASES = {
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -182,7 +181,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
