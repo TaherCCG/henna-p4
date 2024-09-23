f5ac0ba76e14c750538e37dbbf82d2eafe64e6d4
Taher Mahmood <taher.ccg@gmail.com>
2024-09-23 09:45:47 +0000

    Replaced SECRET_KEY

--- henna_store/settings.py
+++ henna_store/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 if os.path.exists("env.py"):
     import env
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(',')
 
