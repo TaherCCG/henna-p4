cc29925688e8208f5037df60f7bbd2552ae5fe61
Taher Mahmood <taher.ccg@gmail.com>
2024-09-24 16:07:19 +0000

    FIx: Static Root

--- henna_store/settings.py
+++ henna_store/settings.py
@@ -200,7 +200,7 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 # else:
 #     STATIC_URL = '/static/'
 #     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #     MEDIA_URL = '/media/'
 #     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
