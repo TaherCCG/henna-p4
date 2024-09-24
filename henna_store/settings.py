3e1eb7ac4be115a076aa909159ddd34fb95493fc
Taher Mahmood <taher.ccg@gmail.com>
2024-09-24 16:54:54 +0000

    Static Root

--- henna_store/settings.py
+++ henna_store/settings.py
@@ -177,6 +177,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
                                      
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
